24e3a3b35e1b3266e8bac6128d216c4665ef65b5
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-01-15 11:03:46 +0100

    feat(AttachKnee): allow to hide the knee model

--- src/controller/states/AttachKnee.cpp
+++ src/controller/states/AttachKnee.cpp
@@ -6,32 +6,34 @@
 void AttachKnee::start(mc_control::fsm::Controller & ctl)
 {
   output("OK");
-  auto & kneeBraceRobot = ctl.loadRobot(mc_rbdyn::RobotLoader::get_robot_module("BoneTag::KneeBrace"), "knee_brace");
-  auto & tibiaRobot = ctl.robot("panda_tibia");
-  if(kneeBraceRobot.hasFrame("KneeBraceCenter") && tibiaRobot.hasFrame("Tibia"))
+  if(config_("display", true))
   {
-    kneeBraceRobot.posW(
-        kneeBraceRobot.frame("KneeBraceCenter").X_b_f().inv() * ctl.robot("panda_tibia").frame("Tibia").position());
+    auto & kneeBraceRobot = ctl.loadRobot(mc_rbdyn::RobotLoader::get_robot_module("BoneTag::KneeBrace"), "knee_brace");
+    auto & tibiaRobot = ctl.robot("panda_tibia");
+    if(kneeBraceRobot.hasFrame("KneeBraceCenter") && tibiaRobot.hasFrame("Tibia"))
+    {
+      kneeBraceRobot.posW(kneeBraceRobot.frame("KneeBraceCenter").X_b_f().inv()
+                          * ctl.robot("panda_tibia").frame("Tibia").position());
+    }
   }
 }
 
 bool AttachKnee::run(mc_control::fsm::Controller & ctl)
 {
+  if(!config_("display", true)) return true;
   if(config_("followKnee", true))
   {
     auto & kneeBraceRobot = ctl.robot("knee_brace");
     auto & tibiaRobot = ctl.robot("panda_tibia");
     if(kneeBraceRobot.hasFrame("KneeBraceCenter") && tibiaRobot.hasFrame("Tibia"))
     {
-      kneeBraceRobot.posW(
-        kneeBraceRobot.frame("KneeBraceCenter").X_b_f().inv() * ctl.robot("panda_tibia").frame("Tibia").position());
+      kneeBraceRobot.posW(kneeBraceRobot.frame("KneeBraceCenter").X_b_f().inv()
+                          * ctl.robot("panda_tibia").frame("Tibia").position());
     }
   }
   return true;
 };
 
-void AttachKnee::teardown(mc_control::fsm::Controller & ctl)
-{
-};
+void AttachKnee::teardown(mc_control::fsm::Controller & ctl){};
 
 EXPORT_SINGLE_STATE("AttachKnee", AttachKnee)
