6d7b43a954f7b1eacf2d035d9d03bcca6f5e2350
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-03-05 12:40:21 +0100

    feat: Add SimulationFast + convex size for knee brace

--- src/controller/states/ChooseMode.cpp
+++ src/controller/states/ChooseMode.cpp
@@ -14,6 +14,12 @@ void ChooseMode::start(mc_control::fsm::Controller & ctl)
                                               ctl.datastore().assign("Mode", std::string{"simulation"});
                                               output("Simulation");
                                             }),
+                        mc_rtc::gui::Button("SimulationFast",
+                                            [&ctl, this]()
+                                            {
+                                              ctl.datastore().assign("Mode", std::string{"simulation_fast"});
+                                              output("SimulationFast");
+                                            }),
                         mc_rtc::gui::Button("Real",
                                             [&ctl, this]()
                                             {
@@ -32,6 +38,7 @@ void ChooseMode::teardown(mc_control::fsm::Controller & ctl)
   // FIXME (mc_rtc) only deletes one element instead of all
   // ctl.gui()->removeElements({}, this);
   ctl.gui()->removeElement({}, "Simulation");
+  ctl.gui()->removeElement({}, "SimulationFast");
   ctl.gui()->removeElement({}, "Real");
 }
 
